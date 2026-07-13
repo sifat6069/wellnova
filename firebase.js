a1886409e306ecec0e86e06d221e8dabf4f6f3d4
sifat6069 <salmaakter1584@gmail.com>
2026-07-13 13:58:16 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,46 +1,60 @@
-// Firebase v10 Modules
+// Firebase Configuration
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
 import {
-getAuth,
-GoogleAuthProvider,
-signInWithPopup,
-signOut
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 import {
-getFirestore
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    updateDoc,
+    deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-
-import {
-getStorage
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyDovsFKGUWXA7hMGznysbweWz3pb_957m0",
+    apiKey: "AIzaSyDovsFKGUWXA7hMGznysbweWz3pb_957m0",
 
-authDomain: "wellnova-419f3.firebaseapp.com",
+    authDomain: "wellnova-419f3.firebaseapp.com",
 
-projectId: "wellnova-419f3",
+    projectId: "wellnova-419f3",
 
-storageBucket: "wellnova-419f3.firebasestorage.app",
+    storageBucket: "wellnova-419f3.firebasestorage.app",
 
-messagingSenderId: "744778511997",
+    messagingSenderId: "744778511997",
 
-appId: "1:744778511997:web:9553d13ff5d9dd5e16a314"
+    appId: "1:744778511997:web:9553d13ff5d9dd5e16a314"
 
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const auth = getAuth(app);
 
-export const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
 
-export const storage = getStorage(app);
-
-export { signInWithPopup, signOut };
+export {
+    auth,
+    db,
+    provider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged,
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    updateDoc,
+    deleteDoc
+};
