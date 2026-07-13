834214c815231aee5ea3740dcfaf88b7f708abb6
sifat6069 <salmaakter1584@gmail.com>
2026-07-13 12:18:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+// =============================
+// WallNova v1.0
+// =============================
+
 console.log("WallNova Loaded");
+
+// Search Box
+const searchInput = document.querySelector(".search-box input");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("keyup", function () {
+
+    const value = this.value.toLowerCase();
+
+    cards.forEach(card => {
+
+        const title = card.querySelector("h3").textContent.toLowerCase();
+
+        if(title.includes(value)){
+            card.style.display = "block";
+        }else{
+            card.style.display = "none";
+        }
+
+    });
+
+});
+
+
+// Category Button Animation
+
+const buttons = document.querySelectorAll(".categories button");
+
+buttons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+buttons.forEach(btn=>btn.classList.remove("active"));
+
+button.classList.add("active");
+
+});
+
+});
+
+
+// Download Button
+
+const downloadButtons=document.querySelectorAll(".card a");
+
+downloadButtons.forEach(btn=>{
+
+btn.addEventListener("click",function(e){
+
+e.preventDefault();
+
+alert("Download Feature Coming Soon 🚀");
+
+});
+
+});
