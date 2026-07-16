9a44b170cb6ee57da1c92fdcfd1aae4f13783329
sifat6069 <salmaakter1584@gmail.com>
2026-07-16 17:47:23 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,34 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-// Firebase Config
+import {
+    getAuth,
+    GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDovsFKGUWXA7hMGznysbweWz3pb_957m0",
-  authDomain: "wellnova-419f3.firebaseapp.com",
-  projectId: "wellnova-419f3",
-  storageBucket: "wellnova-419f3.firebasestorage.app",
-  messagingSenderId: "744778511997",
-  appId: "1:744778511997:web:9553d13ff5d9dd5e16a314",
-  measurementId: "G-83MCD9R2CP"
+
+    apiKey: "YOUR_API_KEY",
+
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+    projectId: "YOUR_PROJECT_ID",
+
+    storageBucket: "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId: "YOUR_SENDER_ID",
+
+    appId: "YOUR_APP_ID"
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+
 export const provider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
